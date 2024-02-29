b2edf07b8d229d5ce7c1ab33abfea13aade49298
rajkanya-chaudhary <dc5068786@gmail.com>
2024-02-29 16:37:20 +0530

    this is a swapping number

---
+++ Swapnumber.java
@@ -0,0 +1,12 @@
+public class Swapnumber {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int temp = 0;
+        System.out.println("Before swapping number = " + a + " , " +b);
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println("After swapping number = " + a + " , " +b);
+    }
+}
