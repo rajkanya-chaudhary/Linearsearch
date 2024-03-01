7f828deecbf16d282f583a1d216e1906ae2995b4
rajkanya-chaudhary <dc5068786@gmail.com>
2024-03-01 17:40:23 +0530

    this is a target print which index

---
+++ Printtargetarray.java
@@ -0,0 +1,25 @@
+public class Printtargetarray {
+    public static int[] printtarget(int arr[],int target)
+    {
+        for(int i = 0;i<arr.length;i++)
+        {
+            for(int j = i+ 1;j<arr.length;j++)
+            {
+                if(arr[i] + arr[j] == target)
+                {
+                    int a[] = {i,j};
+                    return a;
+                }
+            }
+        }
+        return null;
+
+
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,6,5,3,7};
+        int target = 9;
+        printtarget(arr, target);
+        System.out.println(printtarget(arr, target));
+    }
+}
